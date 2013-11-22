40260946e4cdd7656c76a20da0b8582f05fe5f29
Youcef Amine Benabbas <amine.benabbas@googlemail.com>
2013-11-22 09:41:57 +0100

    - Added support for console output (STDOUT).

--- ml-analyze.py
+++ ml-analyze.py
@@ -21,6 +21,10 @@ def main():
 
     parser.add_option("-f","--format", action="store", type="string",
                       dest="format", default="txt")
+
+    parser.add_option("--nw","--no-window", action="store_true",
+                      dest="nw", default=False, help="Don't use the GUI")
+
     
     (options, args) = parser.parse_args()
 
@@ -30,15 +34,22 @@ def main():
     dirAnalyser = DirAnalyser(options.textDir, options.lang, options.format)
     analyserList = dirAnalyser.analyse(attributes)
 
-    app = QApplication(sys.argv)
-    app.setApplicationName('multiple language translationese')
-    
-    dataModel = AnalysisData(analyserList, attributes) 
-    mainWindow = MainWindow(dataModel)
-    mainWindow.update()
-    mainWindow.show()
+    if options.nw:
+        print repr("File Name"), repr(attributes[0])
+        for a in analyserList:
+            print repr(a.fileName), \
+                repr("{0:.3f}".format(a.getResult(attributes[0])))
 
-    sys.exit(app.exec_())
+    else:
+        app = QApplication(sys.argv)
+        app.setApplicationName('multiple language translationese')
+    
+        dataModel = AnalysisData(analyserList, attributes) 
+        mainWindow = MainWindow(dataModel)
+        mainWindow.update()
+        mainWindow.show()
+
+        sys.exit(app.exec_())
 
 
 if __name__=='__main__':
